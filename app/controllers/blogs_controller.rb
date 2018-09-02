aae809d3a1fcc9aa7055aad304d013fd77735b8d
n3m3sis42 <n3m3sis42@gmail.com>
2018-09-02 19:49:40 -0400

    Integrated helper methods from ActionView

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
-  
+
   # GET /blogs
   # GET /blogs.json
   def index
@@ -77,6 +77,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
